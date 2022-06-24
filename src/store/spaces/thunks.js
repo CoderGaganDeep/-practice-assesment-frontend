0dd759309f4b300678a687dd024b1f7d5a0d5665
gagandeepkatodia <gagandeepkatodia@gmail.com>
2022-06-24 11:41:54 +0200

    Feature 2

--- src/store/spaces/thunks.js
+++ src/store/spaces/thunks.js
@@ -1,19 +1,31 @@
-import { apiUrl, DEFAULT_PAGINATION_LIMIT } from "../../config/constants";
+import { apiUrl } from "../../config/constants";
 import axios from "axios";
 import { fetchSpacesSuccess } from "./slice";
-export const FETCH_SPACES_SUCCESS = "FETCH_SPACES_SUCCESS";
-export const SPACE_DETAILS_FETCHED = "SPACE_DETAILS_FETCHED";
+// export const FETCH_SPACES_SUCCESS = "FETCH_SPACES_SUCCESS";
+// export const SPACE_DETAILS_FETCHED = "SPACE_DETAILS_FETCHED";
 
 export const fetchSpaces = () => {
   return async (dispatch, getState) => {
     try {
-      const spacesCount = getState().spaces.allSpaces.length;
-      const response = await axios.get(
-        `${apiUrl}/space?limit=${DEFAULT_PAGINATION_LIMIT}&offset=${spacesCount}`
-      );
-
+      const response = await axios.get(`${apiUrl}/space`);
       console.log(response);
       dispatch(fetchSpacesSuccess(response.data));
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+};
+
+//1. write a thunk to fetch space by id
+export const fetchSpaceById = (id) => {
+  return async (dispatch, getState) => {
+    try {
+      //2. make an axios request to `/${apiUrl}/space/${id}`
+      const response = await axios.get(`${apiUrl}/space/${id}`);
+      //3. console.log the response
+      console.log(response);
+      //4. go to component and import this functions there
+      //5. if you saw console.log, dispatch this action: dispatch(fetchSpacesSuccess(response.data));
     } catch (e) {
       console.log(e.message);
     }
