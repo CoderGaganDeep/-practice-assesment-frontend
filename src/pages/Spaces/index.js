697fe78f133fd55973048c50c395c87d109ae382
gagandeepkatodia <gagandeepkatodia@gmail.com>
2022-06-24 10:38:07 +0200

    Feature 1 complete

--- src/pages/Spaces/index.js
+++ src/pages/Spaces/index.js
@@ -1,1 +1,36 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// import Jumbotron from "react-bootstrap/Jumbotron";
+// import Container from "react-bootstrap/Container";
+import { fetchSpaces } from "../../store/spaces/thunks";
+import { selectSpaces } from "../../store/spaces/selectors";
+import Space from "../../components/Space";
 
+export default function Spaces() {
+  const dispatch = useDispatch();
+  const spaces = useSelector(selectSpaces);
+
+  useEffect(() => {
+    dispatch(fetchSpaces());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Spaces from page</h1>
+
+      {spaces.map((space) => {
+        return (
+          <Space
+            key={space.id}
+            id={space.id}
+            title={space.title}
+            description={space.description}
+            backgroundColor={space.backgroundColor}
+            color={space.color}
+            showLink={true}
+          />
+        );
+      })}
+    </>
+  );
+}
